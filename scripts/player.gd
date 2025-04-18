61504a511248df6033d9720537367f70c3ac3f8c
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-04-17 23:02:03 -0300

    state machine for adjustment move player

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,43 +6,60 @@ const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D as SwicthAnimation
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor() and controls.player_index == 0:
-		velocity += get_gravity() * delta
+enum State { idle, run, stopping, jump, fall }
+
+var _current_state = State.idle
+
+func _set_state(newState: State):
+	if newState == _current_state:
+		return
+	_current_state = newState
 		
-	if not is_on_floor() and controls.player_index == 1:
-		velocity += get_gravity() * delta
 
-
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-
-	if(controls.player_index == 0 ):
-			# Handle jump.
-		if Input.is_action_just_pressed(controls.jump) and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+func handle_player_move(delta):
+	var direction := Input.get_axis(controls.move_left, controls.move_right)
 		
-		var direction := Input.get_axis(controls.move_left, controls.move_right)
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	match _current_state:
+		State.idle:
+			if direction != 0:
+				_set_state(State.run)
+			else:
+				_set_state(State.stopping)
 			
-		animated_sprite_2d.trigger_animation(velocity, direction)
-		move_and_slide()
-	elif controls.player_index == 1:
-		# Handle jump.
-		if Input.is_action_just_pressed(controls.jump) and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		
-		var direction := Input.get_axis(controls.move_left, controls.move_right)
-		if direction:
+			if Input.is_action_just_pressed(controls.jump) and is_on_floor():
+				_set_state(State.jump)
+			elif not is_on_floor():
+				_set_state(State.fall)
+		State.run:
 			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
 			
-		animated_sprite_2d.trigger_animation(velocity, direction)
-		move_and_slide()
+			if Input.is_action_just_pressed(controls.jump) and is_on_floor():
+				_set_state(State.jump)
+			elif not is_on_floor():
+				_set_state(State.fall)
+		State.jump:				
+			velocity.y = JUMP_VELOCITY
+			_set_state(State.fall)
+			
+			
+		State.stopping:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			_set_state(State.idle)
+		
+		State.fall:
+			velocity += get_gravity() * delta
+			if is_on_floor():
+				_set_state(State.idle)
+			
+			
+	animated_sprite_2d.trigger_animation(velocity, direction)
+			
+
+func _physics_process(delta: float) -> void:	
+	## Gavidade aplicada
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+	
+	handle_player_move(delta)
+	
+	move_and_slide()
