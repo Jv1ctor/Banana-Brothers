6de769492ee91af112e866441f7d09bd2bc6260f
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-05-05 05:59:10 -0300

    project add

--- scripts/switch_animation.gd
+++ scripts/switch_animation.gd
@@ -6,14 +6,11 @@ enum State { idle, run, stopping, jump, fall }
 
 func trigger_animation(velocity: Vector2, direction: int, state: State):
 	
-
-	
-	
-
 	if scale.x == 1 && sign(velocity.x) == -1:
 		scale.x = -1
 	elif scale.x == -1 && sign(velocity.x) == 1:
 		scale.x = 1
+		
 	if not get_parent().is_on_floor():
 		play("jump")
 	elif velocity.x != 0 and not state == State.jump:
