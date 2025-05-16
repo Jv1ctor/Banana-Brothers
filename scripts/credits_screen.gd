e3f3067f7603ed78e5434ba1b41f230e759f8d61
Breno Sampaio <brenosg2310@gmail.com>
2025-05-15 22:50:16 -0300

    Plataformas, Funções para controle

--- scripts/credits_screen.gd
+++ scripts/credits_screen.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+@onready var voltar_button = $ExitCreditsButton
+var bola_ja_apertado = false
+
+func _ready() -> void:
+	voltar_button.grab_focus()
+
+func _process(_delta: float) -> void:
+	var bola_pressionado = Input.is_joy_button_pressed(0, JOY_BUTTON_B)
+
+	if bola_pressionado and not bola_ja_apertado:
+		voltar_button.pressed.emit()
+
+	bola_ja_apertado = bola_pressionado
 
 func _on_exit_credits_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://ui/start_screen.tscn")
+	LoadManager.load_scene("res://ui/start_screen.tscn")
--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -3,6 +3,16 @@ extends Node2D
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @export var sound_path: String = "res://sounds/startMusic.mp3"
 
+@onready var start_button = $StartButton
+@onready var credits_button = $CreditsButton
+@onready var exit_button = $ExitButton
+
+var posicao_vertical = 0  # 0 = cima ("Jogar"), 1 = baixo ("Créditos/Sair")
+var posicao_horizontal = 0  # 0 = esquerda ("Créditos"), 1 = direita ("Sair")
+
+var cooldown = 0.2
+var tempo_espera = 0.0
+
 func _ready() -> void:
 	if sound_path != "":
 		var sound = load(sound_path)
@@ -11,13 +21,68 @@ func _ready() -> void:
 	if audio_player.stream:
 		audio_player.play()
 
+	# Foco inicial no botão "Jogar"
+	start_button.grab_focus()
+
+func _process(delta: float) -> void:
+	if tempo_espera > 0:
+		tempo_espera -= delta
+		return
+
+	# Navegação analógica
+	var eixo_y = Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
+	var eixo_x = Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
+
+	if eixo_y < -0.5:
+		mover_vertical(-1)
+	elif eixo_y > 0.5:
+		mover_vertical(1)
+	elif eixo_x < -0.5 and posicao_vertical == 1:
+		mover_horizontal(-1)
+	elif eixo_x > 0.5 and posicao_vertical == 1:
+		mover_horizontal(1)
+
+	# Botão X do controle PS4 (JOY_BUTTON_A = 1)
+	if Input.is_joy_button_pressed(0, JOY_BUTTON_A):
+		executar_botao()
+
+func mover_vertical(direcao: int) -> void:
+	posicao_vertical = clamp(posicao_vertical + direcao, 0, 1)
+
+	if posicao_vertical == 0:
+		start_button.grab_focus()
+	else:
+		if posicao_horizontal == 0:
+			credits_button.grab_focus()
+		else:
+			exit_button.grab_focus()
+
+	tempo_espera = cooldown
+
+func mover_horizontal(direcao: int) -> void:
+	posicao_horizontal = clamp(posicao_horizontal + direcao, 0, 1)
+
+	if posicao_horizontal == 0:
+		credits_button.grab_focus()
+	else:
+		exit_button.grab_focus()
+
+	tempo_espera = cooldown
+
+func executar_botao():
+	if posicao_vertical == 0:
+		start_button.pressed.emit()
+	elif posicao_vertical == 1:
+		if posicao_horizontal == 0:
+			credits_button.pressed.emit()
+		else:
+			exit_button.pressed.emit()
+
 func _on_start_button_pressed() -> void:
 	LoadManager.load_scene("res://levels//world_1.tscn")
 
-
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_credits_button_pressed() -> void:
 	LoadManager.load_scene("res://ui//credits_screen.tscn")
